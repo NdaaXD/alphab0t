53a10978c8c2c8f6e984e91a455ab7fb7bcc276d
NdaaXD <ndaatzy123@gmail.com>
2022-02-23 04:16:42 +0700

    coli

---
+++ shop/donasi.js
@@ -0,0 +1,11 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+//kasih donasi lu di sini ngentod
+
+Pulsa:6288220118685
+
+*MAKASIH BUAT YG UDAH DONASI🙏*
+	}
+
+exports.donasibot = donasibot
+ 
